e4e8fd90bc27d64769d568e49c76d8a8b0261149
MilesMena <miles.mena@colorado.edu>
2025-04-07 14:29:34 -0600

    Update the prompting and filling in the prompts

--- battle_of_wits.py
+++ battle_of_wits.py
@@ -16,9 +16,11 @@ def time_me(func):
         return result
 
 class BattleOfWits():
-    def __init__(self, model, location):
+    def __init__(self, model, location, defend_disp,ask_belief):
         self.model = model
         self.location = location
+        self.defending_disposition = defend_disp
+        self.asking_belief = ask_belief
         # self.questions = ""
         # self.response = ""
         # Relative Path here:
@@ -29,6 +31,8 @@ class BattleOfWits():
         self.csv_path = os.path.join(results_path, file_name)
         self.ensure_csv_header()
         self.exec_times = []
+        print(f"Initialized with model={model}, location={location}, defending_disposition={defend_disp}, asking_belief={ask_belief}")
+
 
     def ensure_csv_header(self):
         # Check if the CSV file exists and has content
@@ -36,14 +40,14 @@ class BattleOfWits():
         if not file_exists or os.stat(self.csv_path).st_size == 0:
             with open(self.csv_path, mode='w', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow(["Iteration", "Execution Time (s)", "Questions", "Response", "Answer"])
+                writer.writerow(["Iteration", "Execution Time (s)", "Location", "Defending Disposition", "Asking Belief", "Questions", "Response", "Answer"])
 
     def read_txt(self, filename):
         with open(filename, "r") as file:
             content = file.read()
         return content
     
-    def replace_prompt_vars(self, prompt, vars_dict):
+    def replace_prompt_vars(self, prompt, vars_dict = {}):
         filled = prompt
         if "[LOCATION]" in filled:
             filled = filled.replace("[LOCATION]", vars_dict['location'])
@@ -51,6 +55,10 @@ class BattleOfWits():
             filled = filled.replace("[QUESTIONS]", vars_dict['questions'])
         if "[RESPONSE]" in filled:
             filled = filled.replace("[RESPONSE]", vars_dict['response'])
+        if "[DISPOSITION]" in filled:
+            filled = filled.replace("[DISPOSITION]", self.defending_disposition)
+        if "[BELIEF]" in filled:
+            filled = filled.replace("[BELIEF]", self.asking_belief)
         return filled
 
     ## Can't be named chat bc ollama
@@ -60,24 +68,29 @@ class BattleOfWits():
         return response['message']['content']
     
     def single_battle(self, iter):
-        start_time = time.time()  # Record the start time
-        gen_questions_prompt = self.read_txt("prompts/prompt_agent2_ask.txt")
-        questions = self.send_chat(gen_questions_prompt)
-        # ANSWER Question
-        ans_q_prompt = self.read_txt("prompts/prompt_agent1.txt")
-        filled_ans_q_prompt = self.replace_prompt_vars(ans_q_prompt, {'location': self.location, 'questions': questions})
-        response = self.send_chat(filled_ans_q_prompt)
-        # PICK box
-        pick_box_prompt = self.read_txt("prompts/prompt_agent2_pick.txt")
-        filled_pick_box_prompt = self.replace_prompt_vars(pick_box_prompt, {'questions':questions, 'response':response})
-        answer = self.send_chat(filled_pick_box_prompt)
-        end_time = time.time()  # Record the end time
-        execution_time = end_time - start_time
-        # self.exec_times.append(execution_time)
-        print(f"Iter: {iter}  | exec time: {execution_time:.6f}s")
-        # Write data to CSV
-        return [iter,f"{execution_time:.6f}",self.sanitize(questions), self.sanitize(response), self.sanitize(answer)]
-    
+        try:
+            start_time = time.time()  # Record the start time
+            gen_questions_prompt = self.read_txt("prompts/prompt_agent2_ask.txt")
+            filled_gen_question_prompt = self.replace_prompt_vars(gen_questions_prompt)
+            questions = self.send_chat(filled_gen_question_prompt)
+            # ANSWER Question
+            ans_q_prompt = self.read_txt("prompts/prompt_agent1.txt")
+            filled_ans_q_prompt = self.replace_prompt_vars(ans_q_prompt, {'location': self.location, 'questions': questions})
+            response = self.send_chat(filled_ans_q_prompt)
+            # PICK box
+            pick_box_prompt = self.read_txt("prompts/prompt_agent2_pick.txt")
+            filled_pick_box_prompt = self.replace_prompt_vars(pick_box_prompt, {'questions':questions, 'response':response})
+            answer = self.send_chat(filled_pick_box_prompt)
+            end_time = time.time()  # Record the end time
+            execution_time = end_time - start_time
+            # self.exec_times.append(execution_time)
+            print(f"Iter: {iter}  | Location: {self.location} | Defending: {self.defending_disposition} | Asking: {self.asking_belief} | exec time: {execution_time:.6f}s")
+            # Write data to CSV
+            return [iter,f"{execution_time:.6f}",self.location, self.defending_disposition, self.asking_belief, self.sanitize(questions), self.sanitize(response), self.sanitize(answer)]
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+
     def multi_battle(self, n):
         for i in range(n):
             result = self.single_battle(i)
@@ -109,8 +122,9 @@ class BattleOfWits():
 
 
 if __name__ == "__main__":
-
-    bw = BattleOfWits("gemma:2b", "A")
+    dispositions = ["Truthful", "Deceitful"]
+    locations = ["A","B"]
+    bw = BattleOfWits("gemma:2b", locations[0], dispositions[0], dispositions[0])
 
 
     # bw.multi_battle(5)
